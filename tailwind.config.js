34131346f39b6a0a143c6217942a5359551149e5
Brian Aung <brianaung16@gmail.com>
2023-12-17 20:57:07 +1100

    add some styles(for ux) with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./ui/**/*.html"],
   theme: {
+    screens: {
+      mobile: "360px",
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
     extend: {},
   },
+  corePlugins: {
+    preflight: false,
+  },
   plugins: [],
-}
-
+};
